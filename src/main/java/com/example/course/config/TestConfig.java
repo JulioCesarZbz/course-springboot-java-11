755cf2d8c0d2996282f7e9668f2aee765525e4ce
JulioCesarZbz <juliocesar_tkd@hotmail.com>
2021-03-31 00:51:29 -0300

    OrderItem, many-to-many association with extra attrributes Part 2

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.course.entities.Category;
 import com.example.course.entities.Order;
+import com.example.course.entities.OrderItem;
 import com.example.course.entities.Product;
 import com.example.course.entities.User;
 import com.example.course.entities.enums.OrderStatus;
 import com.example.course.repositories.CategoryRepository;
+import com.example.course.repositories.OrderItemRepository;
 import com.example.course.repositories.OrderRepository;
 import com.example.course.repositories.ProductRepository;
 import com.example.course.repositories.UserRepository;
@@ -21,6 +23,7 @@ import com.example.course.repositories.UserRepository;
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
+	//aqui serão feitas as injeções de repositories
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -32,7 +35,10 @@ public class TestConfig implements CommandLineRunner {
 	private CategoryRepository categoryRepository;
 	
 	@Autowired
-	private ProductRepository ProductRepository;
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -49,7 +55,7 @@ public class TestConfig implements CommandLineRunner {
 		
 		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		ProductRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
 		//associando os produtos as suas categorias
 		p1.getCategories().add(cat2);
@@ -60,9 +66,7 @@ public class TestConfig implements CommandLineRunner {
 		p5.getCategories().add(cat2);
 		
 		//salvando as associações
-		ProductRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
-		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "999999888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "999999777", "123456");
@@ -71,13 +75,16 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1); 
 		
-		
-		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p4.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p1.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
-	
-	
+
 	
 }
